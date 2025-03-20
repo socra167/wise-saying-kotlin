6859eee4746ca5368f79be29aca4b047d4942814
socra167 <socra167@gmail.com>
2025-03-20 15:20:16 +0900

    feat: 입력과 종료 기능 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package com
 
 fun main() {
-
+    println("== 명언 앱 ==")
+    while (true) {
+        print("입력)")
+        val input = readlnOrNull() ?: "" // null 이 들어올 수 있는 입력을 받는다. elvis 로 null 일 경우 값 지정
+        if (input == "종료") {
+            break
+        }
+    }
 }
