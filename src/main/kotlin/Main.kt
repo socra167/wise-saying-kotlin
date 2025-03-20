0656a7a5d1509fc487138e9765c536c097f476da
socra167 <socra167@gmail.com>
2025-03-20 15:27:48 +0900

    feat: 명언을 입력해 등록 기능 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,18 @@ fun main() {
     println("== 명언 앱 ==")
     while (true) {
         print("입력)")
-        val input = readlnOrNull() ?: "" // null 이 들어올 수 있는 입력을 받는다. elvis 로 null 일 경우 값 지정
-        if (input == "종료") {
-            break
+        val input = readlnOrNull() ?: ""
+
+        when (input) {
+            "종료" -> break;
+            "등록" -> {
+                print("명언: ")
+                val saying = readlnOrNull() ?: ""
+                print("작가: ")
+                val author = readlnOrNull() ?: ""
+
+                println("$saying : $author 명언이 등록되었습니다.")
+            }
         }
     }
 }
