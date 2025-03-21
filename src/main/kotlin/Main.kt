28cfa03c6bfea3b16150877e85dfcd25f37963cb
socra167 <socra167@gmail.com>
2025-03-21 09:26:43 +0900

    feat: , 삭제 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,22 @@
 package com
 
 import com.domain.wisesaying.entity.WiseSaying
+import com.global.Request
 
 fun main() {
 
     val wiseSayings = mutableListOf<WiseSaying>()
 
-    var lastId = 0
+    var lastId = 1
 
     println("== 명언 앱 ==")
     while (true) {
         print("입력)")
         val input = readlnOrNull() ?: ""
 
-        when (input) {
+        val rq = Request(input)
+
+        when (rq.actionName) {
             "종료" -> break
             "등록" -> {
                 print("명언: ")
@@ -26,12 +29,26 @@ fun main() {
                 println("${lastId + 1}번 $author : $saying 명언이 등록되었습니다.")
                 lastId++
             }
+
             "목록" -> {
                 println("번호 / 작가 / 명언")
                 println("-------------------")
                 wiseSayings.reversed().forEach {
                     println("${it.id + 1} / ${it.author} / ${it.saying}")
                 }
+            }
+
+            "삭제" -> {
+                rq.getParam("id")?.let {
+                    it.toIntOrNull()?.let {
+                        wiseSayings.removeIf { saying -> saying.id == it }
+                        println("${it}번 명언을 삭제했습니다.")
+                    } ?: println("올바른 번호를 입력해주세요.")
+                }?: println("삭제할 명언의 번호를 입력해주세요.")
+            }
+
+            else -> {
+                println("명령이 입력되지 않았습니다.")
             }
         }
     }
--- src/main/kotlin/global/Request.kt
+++ src/main/kotlin/global/Request.kt
@@ -1,11 +1,27 @@
 package com.global
 
 class Request(
-    input: String,
+    val input: String,
 ) {
     var actionName: String = ""
-    val params = mutableMapOf<String, String>()
-    init {
+    val paramMap = mutableMapOf<String, String>()
 
+    init {
+        val inputBits = input.split("?", limit = 2)
+        actionName = inputBits[0]
+
+        if (inputBits.size == 2) {
+            val params = inputBits[1].split("&")
+            params.forEach {
+                val paramBits = it.split("=", limit = 2)
+                if (paramBits.size == 2) {
+                    paramMap[paramBits.get(0)] = paramBits.get(1)
+                }
+            }
+        }
+    }
+
+    fun getParam(key: String): String? {
+        return paramMap[key]
     }
 }
