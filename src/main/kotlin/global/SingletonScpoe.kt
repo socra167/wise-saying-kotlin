0b2a476e28e00bcf6a4105326cbb40a61f0f7994
socra167 <socra167@gmail.com>
2025-03-21 14:18:22 +0900

    chore: `SingletonScope` 초기화에 lazy 적용

--- src/main/kotlin/global/SingletonScpoe.kt
+++ src/main/kotlin/global/SingletonScpoe.kt
@@ -6,8 +6,8 @@ import com.domain.wisesaying.controller.SystemController
 import com.domain.wisesaying.repository.WiseSayingRepository
 
 object SingletonScope {
-    val wiseSayingRepository = WiseSayingRepository()
-    val wiseSayingService = WiseSayingService()
-    val wiseSayingController = WiseSayingController()
-    val systemController = SystemController()
+    val wiseSayingRepository by lazy { WiseSayingRepository() }
+    val wiseSayingService by lazy { WiseSayingService() }
+    val wiseSayingController by lazy { WiseSayingController() }
+    val systemController by lazy { SystemController() }
 }
